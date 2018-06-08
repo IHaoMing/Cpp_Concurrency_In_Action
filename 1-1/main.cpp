6b2bc550e40df7fa8fd04e9531a97e850223a4b1
haoming.wu168@gmail.com <IHaoMing>
2018-06-08 15:56:23 +0800

    1-1 hello world

---
+++ 1-1/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include<thread>
+
+using namespace std;
+
+void hello()
+{
+    std::cout<<"Hello world!"<<std::endl;
+}
+
+int main()
+{
+    std::thread t(hello);
+    t.join();
+
+    return 0;
+}
